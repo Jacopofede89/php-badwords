f4cd31fa62c85ff08af0c0939aa8a0274f62cb27
Jacopofede89 <jacopofede89@gmail.com>
2021-12-17 17:12:10 +0100

    Final Exercise

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>php-badwords</title>
+</head>
+<body>
+  <?php $name="Jacopo"?>
+  <h1>Ciao mi chiamo <?php echo $name?></h1>
+  <h1>Il numero delle lettere è: <?php echo strlen($name);?></h1>
+  <style>
+    body{
+      background-color:black;
+      color:white;
+      text-align:center;
+    }
+  </style>
+</body>
+</html>
